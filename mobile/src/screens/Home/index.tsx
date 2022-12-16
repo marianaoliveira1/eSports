fab58b3462d4d1abd459ea3a0ef44c0960defb5a
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2022-12-16 19:38:05 -0300

    components de texto

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../Heading';
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
-
+        <Image 
+            source={logoImg} 
+            style={styles.logo}
+        />
+        <Heading 
+            title="Encontre o seu duo!"
+            subtitle="Selecione o game que desejar jogar..."
+        />
     </View>
   );
 }
