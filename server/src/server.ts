8cb86f569a97c7cc08b2a55bcce9719587d865b4
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-01-13 16:57:31 -0300

    casos de uso

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,27 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([]);
+});
+
+app.post('/games', (request, response) => {
+    return response.status(201).json([]);
+});
+
+
+app.get('/games/:id/ads', (request, response) => {
     return response.json([
         {id: 1, name: 'Anuncio 1'},
         {id: 2, name: 'Anuncio 2'},
-        {id: 3, name: 'Anuncio 1'},
+        {id: 3, name: 'Anuncio 3'},
     ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    return response.json([]);
 })
 
 app.listen(3333)
